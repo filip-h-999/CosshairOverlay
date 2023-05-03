75063cda57d0b03c2215bc9afb9fc597901bafe8
filip-h-999 <filip.hrenovcik@gmail.com>
2023-05-03 17:17:10 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Crosshair Overlay
+Pygame Crosshair Overlay for Every Game
+
+This is a simple Pygame-based crosshair overlay that can be used in any game running in windowed or borderless mode. The overlay is a transparent Pygame window that is always on top of other windows, making it possible to use it to aim in games that don't provide a crosshair.
+
+How to use:
+
+For making the pygamewindow transparent and show only the crosshair press "n"
+For closing the window press "b"
+
+![image](https://user-images.githubusercontent.com/93609912/235959287-a5fb66ab-5b06-4d8a-a154-c2c6904d3029.png)
+![image](https://user-images.githubusercontent.com/93609912/235960540-5694cc27-de53-47a0-ac19-f80aec3dabd7.png)
---
+++ crosshairOverlay.py
@@ -0,0 +1,79 @@
+import pygame
+import win32api
+import win32con
+import win32gui
+
+
+class CrosshairOverlay:
+    def __init__(self):
+        pygame.init()
+
+        self.width = 200
+        self.height = 200
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        self.is_visible = True
+
+        self.crosshair_color = ('#00FFF4')
+        self.crosshair_thickness = 2
+        self.crosshair_size = 5
+        self.crosshair_pos = (self.width // 2, self.height // 2)
+
+
+    def draw_crosshair(self):
+        pos = self.crosshair_pos
+        size = self.crosshair_size
+        thickness = self.crosshair_thickness
+        color = self.crosshair_color
+        pygame.draw.line(self.screen, color, (pos[0] - size, pos[1]), (pos[0] + size, pos[1]), thickness)
+        pygame.draw.line(self.screen, color, (pos[0], pos[1] - size), (pos[0], pos[1] + size), thickness)
+
+
+    def transparent(self, color):
+        hwnd = pygame.display.get_wm_info()["window"]
+        win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE, win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE) | win32con.WS_EX_LAYERED)
+        win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(*color), 0, win32con.LWA_COLORKEY)
+
+
+    def toggle_visibility(self):
+        self.is_visible = not self.is_visible
+        if self.is_visible:
+            self.screen = pygame.display.set_mode((self.width, self.height))
+
+        else:
+            self.screen = pygame.display.set_mode((self.width, self.height), pygame.NOFRAME)
+            
+            
+    def alwaysOnTop(self):
+        hwnd = win32gui.GetForegroundWindow()
+        screen_width, screen_height = win32api.GetSystemMetrics(0), win32api.GetSystemMetrics(1)
+        x_pos, y_pos = (screen_width - self.width) // 2, (screen_height - self.height) // 2
+        win32gui.SetWindowPos(hwnd, win32con.HWND_TOPMOST, x_pos, y_pos, 0, 0, win32con.SWP_NOSIZE)
+
+
+    def run(self):
+        fuchsia = (255, 0, 128)
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    return
+                
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_b:
+                        pygame.quit()
+                        return
+                        
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_n:
+                        self.toggle_visibility()
+                        self.alwaysOnTop()
+                
+                self.transparent(fuchsia)
+                self.screen.fill(fuchsia)
+                self.draw_crosshair()
+                pygame.display.update()
+
+
+if __name__ == '__main__':
+    overlay = CrosshairOverlay()
+    overlay.run()
